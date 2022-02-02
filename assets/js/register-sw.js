e742a77bb6bedc963c88ee00c0ec4ea59de01155
Mufanc <1714511787@qq.com>
2022-02-03 04:09:50 +0800

    Site updated: 2022-02-03 04:09:50

---
+++ assets/js/register-sw.js
@@ -0,0 +1,13 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistration().then((state) => {
+        if (!state) {
+            navigator.serviceWorker.register('/service-worker.js', { scope: '/' }).then(function(reg) {
+                // registration worked
+                console.log('Registration succeeded. Scope is ' + reg.scope);
+              }).catch(function(error) {
+                // registration failed
+                console.log('Registration failed with ' + error);
+              });
+        }
+    })
+  }
