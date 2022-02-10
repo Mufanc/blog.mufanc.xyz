fc76505b2f837f56c9665d17f117edb1e7cf1518
Mufanc <1714511787@qq.com>
2022-02-10 16:36:30 +0800

    使用 Github Actions 自动合并友链

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## blog.mufanc.xyz
 
-Hi 👋，欢迎来到我的博客！如你所见，我使用 GitHub Pages 来部署它。这里是存放静态文件的仓库，如果你想看博客内容，可以通过这个 [传送门](https://blog.mufanc.xyz) 访问
+&emsp;&emsp;Hi 👋，欢迎来到我的博客！如你所见，我使用 GitHub Pages 来部署它。这里是存放静态文件的仓库，如果你想看博客内容，可以通过这个 [传送门](https://blog.mufanc.xyz) 访问
+
+### 这里也有一些其它功能
+
+* [交换友链](https://github.com/Mufanc/blog.mufanc.xyz/issues/new/choose)
---
+++ index.js
@@ -0,0 +1,50 @@
+const core = require('@actions/core')
+const { context, GitHub } = require('@actions/github')
+const fs = require('fs').promises
+
+!(async function () {
+    try {
+        const token = process.env.GITHUB_TOKEN
+        const body = process.env.ISSUE_BODY
+        const { owner, repo } = context.repo
+
+        // 保存到单独的 json 文件
+        let [ name, link, avatar, descr ] = body.match(/{%.+?%}/g)
+            .map(x => x.substring(2, x.length-2).trim())
+        let domain = link.match(/https?:\/\/([^/]+)(?:\/.*)?/)[1]
+        await fs.writeFile(`./link/sites/${domain}.json`, JSON.stringify({
+            name, link, avatar, descr
+        }))
+
+        // 生成总的友链 json
+        let files = (await fs.readdir('./link/sites')).filter(str => !str.startsWith('.'))
+        let json = JSON.parse((await fs.readFile('./link/template.json')).toString())
+        let target;
+        for (let obj of json) {
+            if ('gh-actions' in obj) {
+                target = obj;
+                delete target['gh-actions']
+                break;
+            }
+        }
+        for (let file of files) {
+            let site = JSON.parse((await fs.readFile(`./link/sites/${file}`)).toString())
+            target['link_list'].push(site)
+        }
+        await fs.writeFile('links.json', JSON.stringify(json))
+
+        // 评论并关闭 issue
+        const octokit = new GitHub(token)
+        let issue_number = context.payload.issue?.number
+        await octokit.issues.createComment({
+            owner, repo, issue_number,
+            body: '恭喜！你提交的友链已经成功合并'
+        })
+        await octokit.issues.update({
+            owner, repo, issue_number,
+            state: 'closed'
+        })
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+})()
